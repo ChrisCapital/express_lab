edd34294046d524396be7918bd5ba6725af9960e
ChrisCapital <chris.shavuo@gmail.com>
2025-11-03 14:04:04 -0500

    added users routes

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express =require('express');
 const app = express(); //calling this function sets up a server
 
+app.set("view engine", "ejs"); //telling express to use ejs as the view engine
+
 app.get('/', (req,res)=>{
     console.log('Here');
-    res.send('Hello World');
-}); // this function wil run when soemone goes to root fl
+    res.render("index", {user: "George"});
+});
+
+app.get('/user', (req,res)=>{
+    res.send("user list");
+}
+
+app.get("/users/new", (req,res)=>{
+    res.send("new user form");
+}
 
 
 
-
-
-
-app.listen(3030); //listening on port 3030
+app.listen(3030); //listening on port 3030 // localhost:3030
 
